1ea973b5f020ef34816434cf72a049389c3223ce
Gabriel Barbosa <gabrielsaba30@hotmail.com>
2023-08-16 15:49:58 -0300

    selecoes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,22 @@ import express from 'express' // importar o express
 
 const app = express() // criar uma instancia dp express
 
+
+// mock
+const selecoes = [
+    {id:1, selecao: 'Brasil', grupo: 'G'},
+    {id:1, selecao: 'Suiça', grupo: 'G'},
+    {id:1, selecao: 'Sérvia', grupo: 'G'},
+    {id:1, selecao: 'Camarões', grupo: 'G'},
+]
+
 // criar rota padrão ou raiz
 app.get('/', (req, res) => {
     res.send('Curso de Node JS')
 })
 
 app.get('/selecoes', (req, res) =>{
-    res.send('Lista de seleções')
+    res.send(selecoes)
 })
 
 export default app // exportando a constante app que é uma instacia do express
